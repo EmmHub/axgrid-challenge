7b77a3386af599336152f9dc190742b90ca6e9ed
eraposo <eraposo@eservicios.indra.es>
2024-04-09 16:29:40 +0200

    feat(EnergyTradeForm): Add toast notifications.
    The commit introduces toast notifications for successful energy trade form submissions. The `react-toastify` package was added to dependencies to handle toasts. Now, a toast message is also displayed after a successful trade creation.

--- frontend/src/components/EnergyTradeForm/index.tsx
+++ frontend/src/components/EnergyTradeForm/index.tsx
@@ -2,9 +2,11 @@ import Form, { IChangeEvent } from '@rjsf/core';
 import { useLocation } from 'react-router-dom';
 import validator from '@rjsf/validator-ajv8';
 import { useRef } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
 import { formSchema } from '../../schemas/form';
 import { useEnergyTrade } from '../../context/EnergyTradeContext';
 import { createTrade, getFormHeader, getUpdatedUiSchema } from '../../utils';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const EnergyTradeForm = () => {
 	const formRef = useRef<Form>(null);
@@ -18,6 +20,7 @@ export const EnergyTradeForm = () => {
 
 	const onSubmit = (data: IChangeEvent) => {
 		addTrade(createTrade(data));
+		toast.success('Trade was successfully created and processed.');
 		resetForm();
 	};
 
@@ -27,6 +30,17 @@ export const EnergyTradeForm = () => {
 	return (
 		<div className='flex justify-center mt-20 mb-10'>
 			<div className='w-3-10'>
+				<ToastContainer
+					position='top-right'
+					autoClose={3000}
+					hideProgressBar
+					newestOnTop={false}
+					closeOnClick
+					rtl={false}
+					pauseOnFocusLoss
+					draggable
+					pauseOnHover
+				/>
 				<h1 className='mb-5'>{formHeader}</h1>{' '}
 				<Form
 					schema={formSchema}

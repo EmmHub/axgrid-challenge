ea51bb186024a10ba35545f40d7ea32132cfb7d5
eraposo <eraposo@eservicios.indra.es>
2024-04-09 23:33:56 +0200

    feat(EnergyTradesTable): add detailed comments to components.
    Introduced detailed documentation to the EnergyTradesTable component. Enhanced the clarity of the renderTableHeaderRow and renderTableRow functions by providing more elaborate comments. Consequently, this commit improves the understanding of the component's structure and functionalities, thereby aiding future development and maintenance.

--- frontend/src/components/EnergyTradesTable/index.tsx
+++ frontend/src/components/EnergyTradesTable/index.tsx
@@ -10,12 +10,25 @@ import {
 } from 'react-table';
 import { SelectColumnFilter } from '../SelectColumnFilter';
 
+/**
+ * Represents an instance of a custom column in a table.
+ *
+ * @template D - The type of the data in the column.
+ */
 interface CustomColumnInstance<D extends object = Record<string, unknown>>
 	extends ColumnInstance<D> {
 	canFilter?: boolean;
 }
 
-const renderTableHeaderRow = (headerGroup: HeaderGroup<TableFormat>) => (
+/**
+ * Renders the table header row using the given header group. Each cell in the header row represents a table column.
+ *
+ * @param {HeaderGroup<TableFormat>} headerGroup - The header group containing column headers.
+ * @returns {React.ReactNode} - The rendered table header row.
+ */
+const renderTableHeaderRow = (
+	headerGroup: HeaderGroup<TableFormat>
+): React.ReactNode => (
 	<tr {...headerGroup.getHeaderGroupProps()}>
 		{headerGroup.headers.map((column) => {
 			const columnHeader = column as CustomColumnInstance<TableFormat>;
@@ -31,10 +44,16 @@ const renderTableHeaderRow = (headerGroup: HeaderGroup<TableFormat>) => (
 	</tr>
 );
 
+/**
+ * Renders a single table row.
+ * @param {Row<TableFormat>} row - The row to render.
+ * @param {(row: Row<TableFormat>) => void} prepareRow - A function to prepare the row before rendering.
+ * @returns {React.ReactElement} - The rendered table row.
+ */
 const renderTableRow = (
 	row: Row<TableFormat>,
 	prepareRow: (row: Row<TableFormat>) => void
-) => {
+): React.ReactElement => {
 	prepareRow(row);
 	return (
 		<tr {...row.getRowProps()}>
@@ -47,10 +66,22 @@ const renderTableRow = (
 	);
 };
 
+/**
+ * React component for displaying an Energy Trades Table.
+ *
+ * @component
+ * @category Components
+ *
+ * @param {EnergyTradesTableProps} props - The properties for the EnergyTradesTable component.
+ * @param {Array.<TableFormat>} props.data - The data for the table.
+ * @param {Column<TableFormat>[]} props.columns - The columns to display in the table.
+ *
+ * @returns {React.ReactElement} - The rendered EnergyTradesTable component.
+ */
 export const EnergyTradesTable: React.FC<EnergyTradesTableProps> = ({
 	data,
 	columns,
-}) => {
+}: EnergyTradesTableProps): React.ReactElement => {
 	const tableProps = { columns, data };
 	const tableInstance = useTable(tableProps, useFilters);
 
